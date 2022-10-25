d4241b58abc3e08f62b2615f3c07ed03011abd70
han <chos9715@naver.com>
2022-10-25 21:38:57 +0900

    날씨 삭제API

--- src/main/java/com/zb/weather/repository/DiaryRepository.java
+++ src/main/java/com/zb/weather/repository/DiaryRepository.java
@@ -3,6 +3,7 @@ package com.zb.weather.repository;
 import com.zb.weather.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,4 +14,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Integer> {
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 
     Diary getFirstByDate(LocalDate date);
+
+    @Transactional
+    void deleteAllByDate(LocalDate date);
 }
